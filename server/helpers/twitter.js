88b3295126e5be703f2d60babadb9e5f97a708a7
Marco Berardini <mberardini@evolution-man.com>
2016-07-05 19:35:50 -0700

    modified twitter.js

--- server/helpers/twitter.js
+++ server/helpers/twitter.js
@@ -1,14 +1,24 @@
 var Twitter = require('twitter');
-var keys  = require('./keys.js');
+//var keys  = require('./keys.js');
 
 console.log("helpers", keys);
+//
+//var client = new Twitter({
+//  consumer_key:  process.env.consumer_key ||  keys.consumer_key,
+//  consumer_secret: process.env.consumer_secret || keys.consumer_secret,
+//  access_token_key:  process.env.access_token_key || keys.access_token_key,
+//  access_token_secret: process.env.access_token_secret || keys.access_token_secret
+//});
+
+
 
 var client = new Twitter({
-  consumer_key:  process.env.consumer_key ||  keys.consumer_key,
-  consumer_secret: process.env.consumer_secret || keys.consumer_secret,
-  access_token_key:  process.env.access_token_key || keys.access_token_key,
-  access_token_secret: process.env.access_token_secret || keys.access_token_secret
+  consumer_key:  process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret ,
+  access_token_key:  process.env.access_token_key ,
+  access_token_secret: process.env.access_token_secret
 });
+
 
 var count = 25;
 
